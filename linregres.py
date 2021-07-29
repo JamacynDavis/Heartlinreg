a969df1d43687d32a1fa6974c81912df66835387
JamacynDavis <52086747+JamacynDavis@users.noreply.github.com>
2021-07-28 20:33:55 -0400

    Deleted unneeded comments

--- linregres.py
+++ linregres.py
@@ -15,107 +15,11 @@ import pandas as pd
 from tkinter import Tk     # from tkinter import Tk for Python 3.x
 from tkinter.filedialog import askopenfilename, asksaveasfile
 
-# Tk().withdraw() # we don't want a full GUI, so keep the root window from appearing
-# filename = askopenfilename() # show an "Open" dialog box and return the path to the selected file
-# print(filename)
-# Read in the given CSV file 
-# fileObject = pd.read_csv(filename)
-# fileObject = pd.DataFrame()
-# fileObject = cudf.read_csv('/data/sample.csv') # leave commented out
-# print(fileObject)
-
-# Indicates the number of dimensions
-# fileObject.shape
-
-# Used for statistical representations of the data 
-# fileObject.describe()
-
-# Prints information like the total number of data entries and null values
-# fileObject.info()
-
-# Import matplotlib 
 import matplotlib.pyplot as plt 
 import seaborn as sns 
 
-# Creating a pairplot of the data using seaborn
-# sns.pairplot(fileObject, x_vars=['age', 'trtbps'], y_vars='thalachh', size = 5, aspect = 1, kind = 'scatter')
-# plt.show()
-
-# Creating a heatmap represenation of the correlation
-# figure = plt.figure(figsize = (10, 10))
-# sns.heatmap(fileObject.corr(), cmap = 'PuBu', annot=True)
-# plt.xlabel('Values on x axis')
-# plt.ylabel('Values on y axis')
-# plt.show() 
-
-# x = fileObject['age']
-# y = fileObject['thalachh']
-
-# Splitting the data into train and test sets (100% random)
-# from sklearn.model_selection import train_test_split
-# x_train, x_test, y_train, y_test = train_test_split(x, y, train_size = 0.7, test_size = 0.3, random_state = 100)
-
-# print("\n")
-# print(x_train)
-# print(y_train)
 
 import statsmodels.api as model 
-# x_train_sm = model.add_constant(x_train)
-
-# lr = model.OLS(y_train, x_train_sm).fit()
-# print(lr.params)
-# print(lr.summary())
-
-# plt.scatter(x_train, y_train)
-# plt.plot(x_train, 201.157201 - 0.949434 * x_train, 'r')
-# plt.show()
-
-#predicting the y values based on the regression line 
-# y_train_pred = lr.predict(x_train_sm)
-
-#residual 
-# res = (y_train - y_train_pred)
-
-# Creating a histogram using the residual values found earlier
-# fig = plt.figure(figsize = (6, 6)) 
-# sns.distplot(res, bins=10)
-# plt.title("Error", fontsize = 11)
-# plt.xlabel('y_train - y_train_pred', fontsize = 10)
-# plt.show()
-
-# making sure no patterns were missed 
-# plt.scatter(x_train, res)
-# plt.show() 
-
-# Calculating r squared 
-# from sklearn.metrics import r2_score 
-# r_2_value = r2_score(y_train, y_train_pred)
-# print('r-squared value: ')
-# print(r_2_value)
-# Now dealing with the test data 
-
-# adding a constant to the test data for x 
-# x_test_sm = model.add_constant(x_test)
-
-# predict 
-# y_test_pred = lr.predict(x_test_sm)
-# print(y_test_pred)
-
-# r_squared  = r2_score(y_test, y_test_pred)
-# print('r squared value: ')
-# print(r_squared)
-
-# plt.scatter(x_test, y_test)
-# plt.plot(x_test, y_test_pred, 'r')
-# plt.show()
-
-# Comparing the r squared value from the train and test data 
-# print('\nThis is the r squared value from the test data: ')
-# print(r_squared)
-# print('This is the r squared data from the train data: ')
-# print(r_2_value)
-
-
 #Attempting to add GUI controls 
 import tkinter as tk 
 from tkinter import * 
